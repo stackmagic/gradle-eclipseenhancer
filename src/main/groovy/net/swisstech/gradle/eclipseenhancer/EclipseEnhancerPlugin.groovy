b9fd3ad0ba6a151d4715aa27325809e06644f8a9
Patrick Huber <stackmagic@gmail.com>
2014-11-10 11:03:06 +0100

    first commit

---
+++ src/main/groovy/net/swisstech/gradle/eclipseenhancer/EclipseEnhancerPlugin.groovy
@@ -0,0 +1,40 @@
+package net.swisstech.gradle.eclipseehancer
+
+import org.slf4j.*
+
+import org.gradle.api.*
+import org.gradle.api.plugins.*
+import org.gradle.api.artifacts.*
+
+/** some enhancements for eclipse */
+class EclipseEnhancerPlugin implements Plugin<Project> {
+
+	void apply(Project project) {
+
+		project.afterEvaluate {
+			project.configure(project) {
+
+				// add plugin if missing, no need to specify enhancer+eclipse itself in
+				// a simple setup. if the user want to do something fancy with the eclipse
+				// plugin, he can - we're running in afterEvaluate anyway
+				if (!project.plugins.hasPlugin('eclipse')) {
+					apply plugin: 'eclipse'
+				}
+
+				// creating all directories eclipse expects to be present
+				eclipseProject.doLast {
+					boolean hasJava   = project.plugins.hasPlugin('java')
+					boolean hasGroovy = project.plugins.hasPlugin('groovy')
+					if (hasJava) {              sourceSets*.java.srcDirs*.each      { it.mkdirs() } }
+					if (hasGroovy) {            sourceSets*.groovy.srcDirs*.each    { it.mkdirs() ; sourceSets.main.java.srcDirs += it } }
+					if (hasJava || hasGroovy) { sourceSets*.resources.srcDirs*.each { it.mkdirs() } }
+				}
+
+				// make sure all output directories are gone
+				project.tasks.maybeCreate('mrproper').doLast {
+					project.delete("${projectDir}/bin")
+				}
+			}
+		}
+	}
+}
